3f2f1a9a18f2efb979e3d58ff6a044a6578fe5a6
Olovan <micahsmith.92@gmail.com>
2016-01-21 21:57:10 -0500

    Added uniform variable to shaders

--- shaders/FragmentShader.glsl
+++ shaders/FragmentShader.glsl
@@ -3,6 +3,8 @@
 in vec3 VertexOutputColors;
 out vec4 FragmentShaderOutputColors;
 
+uniform mat4 orthoMatrix;
+
 void main()
 {
 	FragmentShaderOutputColors = vec4(VertexOutputColors, 1);
--- shaders/VertexShader.glsl
+++ shaders/VertexShader.glsl
@@ -4,6 +4,8 @@ in layout(location = 0) vec2 position;
 in layout(location = 1) vec3 inputColors;
 out vec3 VertexOutputColors;
 
+uniform mat4 orthoMatrix;
+
 void main()
 {
 	VertexOutputColors = inputColors;
