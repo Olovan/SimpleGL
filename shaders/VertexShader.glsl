e7c84cbf17cd4f7584e6d4ee41f6507e94a3f78b
Olovan <micahsmith.92@gmail.com>
2016-01-25 19:07:07 -0500

    Made position a vec3 coordinate for depth support

--- shaders/VertexShader.glsl
+++ shaders/VertexShader.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-layout(location = 0) in vec2 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 inputColors;
 out vec3 VertexOutputColors;
 
@@ -11,7 +11,7 @@ uniform mat4 modelMatrix = mat4(1.0);
 void main()
 {
 	VertexOutputColors = inputColors;
-	vec4 positionVec4 = vec4(position, 0, 1);
+	vec4 positionVec4 = vec4(position, 1);
 	gl_Position = projectionMatrix * viewMatrix * modelMatrix * positionVec4;
 
 }
